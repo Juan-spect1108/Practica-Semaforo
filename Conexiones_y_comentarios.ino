5ab038ef306d66bcb5e0a6ceb19a30580e2b947f
Juan Guzman <63362277+Juan-spect1108@users.noreply.github.com>
2020-06-15 22:25:44 -0500

    Add files via upload

---
+++ Conexiones_y_comentarios.ino
@@ -0,0 +1,34 @@
+// Proyecto para hacer pruebas de un semaforo
+
+// Proyecto diseñado por: Juan Rodrigo Guzman Martinez 
+
+/*
+
+ Materiales:
+ 1. Placa Arduino.
+ 2. LED color rojo.
+ 3. LED color amarillo.
+ 4. LED color verde.
+ 5. 3 resistencias de 220 ohms. 
+ 6. cable para conexiones.
+ 7. Protoboard.
+
+ Conexiones de Arduino al LED rojo 
+    Arduino         LED rojo 
+      13               +
+      GND              -
+
+ Conexiones de Arduino al LED amarillo  
+    Arduino         LED amarillo 
+      12               +
+      GND              -
+
+ Conexiones de Arduino al LED verde 
+    Arduino         LED verde 
+      11               +
+      GND              -    
+
+ Este codigo te permite hacer una implementacion y una prueba de un funcionamiento 
+ de un semaforo en este caso haciendo la prueba con LEDS.
+
+ */
